85348f13eaad49f940c163b4088f3d47a37185c7
junhao chen <junhaochen774@gmail.com>
2025-01-13 10:42:40 +0800

    fix(UI): 修复了无法选择候选值的问题

--- src/components/Board/Cell.svelte
+++ src/components/Board/Cell.svelte
@@ -40,7 +40,7 @@
 			 class:relative-cell={relativeCell}
 		>
 			<button class="cell-btn" on:click={cursor.set(cellX - 1, cellY - 1)}>
-				{#if candidates.length > 1 && strategyCell }
+				{#if candidates.length > 1 && (strategyCell || relativeCell)}
 					<Candidates {candidates} gridRow={cellY - 1} gridCol={cellX - 1}/>
 				{:else if candidates.length === 1}
 					<span class="cell-text">{candidates[0] || ''}</span>
